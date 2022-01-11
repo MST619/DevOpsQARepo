bb5aa494b859460f97be3e43f97b686f44153424
isaiahlowjung <93189824+isaiahlowjung@users.noreply.github.com>
2022-01-11 12:31:03 +0800

    trying stuff out with unit testing

--- test_Index.py
+++ test_Index.py
@@ -21,22 +21,32 @@ def test_gameGrid(capfd, xAxis, yAxis, expectedprint):
     out, _ = capfd.readouterr()
     assert expectedprint in out
 
-# def test_newGame(capfd, monkeypatch, gameGridList):
-#     monkeypatch.setattr('builtins.input', lambda _: "27")
-#     expected = "Invalid Input!\n\
-# Please Select Game Ma Size\n\
-# Enter in your desired map size width (Max:26)"
-#     Index.newGame(gameGridList)
-#     out, _ = capfd.readouterr()
-#     assert out == expected
+def test_newGame(capfd, monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "27", "/")
+    expected = "Invalid Input!\n\
+Please Select Game Ma Size\n\
+Enter in your desired map size width (Max:26)"
+    Index.newGame(gameGridList)
+    out, _ = capfd.readouterr()
+    assert out == expected
 
 
 
 @pytest.mark.parametrize("input, expectedprint", 
                         [("27", "Invalid Input!\n\n" +
                         "Please Select Game Map size\n" +
-                        "Enter in your desired map size width (Max: 26)")])
+                        "Enter in your desired map size width (Max: 26)"), 
+                        ("4", "Enter in your desired map size height (Max: 10):")])
 def test_newGame(capfd, input, expectedprint):
-    Index.newGame(input)
+    Index.newGame(gameGridList)
     out, _ = capfd.readouterr()
     assert expectedprint in out
+
+
+# def test_newGame(capfd, monkeypatch):
+#     monkeypatch.setattr('builtins.input', lambda _: "4")
+#     expected = "Enter in your desired map size height (Max: 10):"
+#     Index.newGame(gameGridList)
+#     out, _ = capfd.readouterr()
+#     assert out == expected
+
